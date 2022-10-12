461d1dbf82c94257304f985e5007ac4d9efee7d9
“MarcioJCosta10” <marciojcostadev@gmail.com>
2022-10-12 18:58:52 -0300

    insert __str__(): in src in models.py and mrun migrate

--- products/models.py
+++ products/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Product(models.Model):
-  title = models.CharField(max_length=255)
-  description = models.TextField(max_length=255)
-  price = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
+    price = models.DecimalField(
+        decimal_places=2, max_digits=20, default=100.00)
+
+    def __str__(self):
+        return self.title
