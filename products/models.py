9a9cebca99fad5a1700ef2af6b1a47a47cffdeb5
“MarcioJCosta10” <marciojcostadev@gmail.com>
2022-10-22 19:45:46 -0300

    insert src/product/detail.html and insert tag image.url in detail.html

--- products/models.py
+++ products/models.py
@@ -3,10 +3,17 @@ from django.db import models
 # Create your models here.
 
 
-class Product(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField(max_length=255)
-    price = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
-    image       = models.FileField(upload_to = 'products/')
+class Product(models.Model):  # product_category
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    price = models.DecimalField(
+        decimal_places=2, max_digits=20, default=100.00)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+
+    # python 3
     def __str__(self):
+        return self.title
+
+    # python 2
+    def __unicode__(self):
         return self.title
