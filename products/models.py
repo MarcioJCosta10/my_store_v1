ea235b50822090f595cf330c0bfc8f2826b8ae5e
“MarcioJCosta10” <marciojcostadev@gmail.com>
2022-10-22 19:03:18 -0300

    insert image field in src/product/models.py

--- products/models.py
+++ products/models.py
@@ -6,8 +6,7 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(max_length=255)
-    price = models.DecimalField(
-        decimal_places=2, max_digits=20, default=100.00)
-
+    price = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
+    image       = models.FileField(upload_to = 'products/')
     def __str__(self):
         return self.title
