06dc58510c37b7407a1be90f5f089c9d7707b90d
“MarcioJCosta10” <marciojcostadev@gmail.com>
2022-10-24 23:00:00 -0300

    insert qs.count in views.py and optimize product_detail_view in src/product/views.py for detail view

--- products/views.py
+++ products/views.py
@@ -4,21 +4,18 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
-# Class Based View
-
-
+#Class Based View
 class ProductListView(ListView):
-    # traz todos os produtos do banco de dados sem filtrar nada
+    #traz todos os produtos do banco de dados sem filtrar nada 
     queryset = Product.objects.all()
     template_name = "products/list.html"
+    
+    #def get_context_data(self, *args, **kwargs):
+        #context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        #print(context)
+        #return context
 
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
-    #     print(context)
-    #     return context
-
-
-# Function Based View
+#Function Based View
 def product_list_view(request):
     queryset = Product.objects.all()
     context = {
@@ -26,30 +23,26 @@ def product_list_view(request):
     }
     return render(request, "products/list.html", context)
 
-# Class Based View
-
-
+#Class Based View
 class ProductDetailView(DetailView):
+    #traz todos os produtos do banco de dados sem filtrar nada 
     queryset = Product.objects.all()
     template_name = "products/detail.html"
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(ProductDetailView, self).get_context_data(
-            *args, **kwargs)
+    
+    def get_context_data(self):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         print(context)
         return context
 
-# Function Based View
-
-
-def product_detail_view(request, pk=None, *args, **kwargs):
-    # instance = Product.objects.get(pk = pk) #get the object id
-    #instance = get_object_or_404(Product, pk=pk)
-
-    try:
-        instance = Product.objects.get(id=pk)
-    except Product.DoesNotExist:
-        print("Nenhum produto encontrado aqui!")
+#Function Based View
+def product_detail_view(request, pk = None, *args, **kwargs):
+    #instance = Product.objects.get(pk = pk) #get the object id
+    #instance = get_object_or_404(Product, pk = pk)
+    qs = Product.objects.filter(id = pk)
+    
+    if qs.count() == 1:
+        instance = qs.first()
+    else:
         raise Http404("Esse produto não existe!")
 
     context = {
