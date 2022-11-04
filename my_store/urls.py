c6c545e7d10f22d0cd401b4fc76b2f68c70a7d98
“MarcioJCosta10” <marciojcostadev@gmail.com>
2022-11-03 21:57:22 -0300

    create src/products/urls.py and set routes and import include in src/my_store/urls.py and reference url in products

--- my_store/urls.py
+++ my_store/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view, ProductFeaturedListView, ProductFeaturedDetailView, ProductDetailSlugView
 
@@ -13,13 +13,7 @@ urlpatterns = [
     path('contact/', contact_page),
     path('login/', login_page),
     path('register/', register_page),
-    path('featured/', ProductFeaturedListView.as_view()),
-    path('featured/<int:pk>/', ProductFeaturedDetailView.as_view()),
-    path('products/', ProductListView.as_view()),
-    path('products-fbv/', product_list_view),
-    path('products/<int:pk>', ProductDetailView.as_view()),
-    path('products-fbv/<int:pk>', product_detail_view),
-    path('products/<slug:slug>/', ProductDetailSlugView.as_view()),
+    path('products/', include('products.urls')),   
     path('admin/', admin.site.urls),
 ]
 
