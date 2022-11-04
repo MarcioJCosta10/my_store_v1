ede371bdf12d149579ce7f2bfe465f87e7ea2b6d
“MarcioJCosta10” <marciojcostadev@gmail.com>
2022-11-03 21:39:03 -0300

    update and refactory code

--- products/models.py
+++ products/models.py
@@ -33,25 +33,16 @@ class ProductManager(models.Manager):
         return None
 
 # Create your models here.
-def get_by_id(self, id):
-    qs = self.get_queryset().filter(id=id)
-    if qs.count() == 1:
-        return qs.first()
-    return None
-
-# Create your models here.
 
 
 class Product(models.Model):  # product_category
     title = models.CharField(max_length=120)
-    slug = models.SlugField(blank = True, unique=True)
+    slug = models.SlugField(blank=True, unique=True)
     description = models.TextField()
-    price = models.DecimalField(
-        decimal_places=2, max_digits=20, default=100.00)
+    price = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
     image = models.FileField(upload_to='products/', null=True, blank=True)
     featured = models.BooleanField(default=False)
     active = models.BooleanField(default=True)
-
     objects = ProductManager()
     # python 3
 
@@ -61,8 +52,11 @@ class Product(models.Model):  # product_category
     # python 2
     def __unicode__(self):
         return self.title
-      
+
+
 def product_pre_save_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:
         instance.slug = unique_slug_generator(instance)
-pre_save.connect(product_pre_save_receiver, sender = Product)
+
+
+pre_save.connect(product_pre_save_receiver, sender=Product)
