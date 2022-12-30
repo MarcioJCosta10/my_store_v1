873d583707e29c43e2b7032d4f01ee2fe0acfabc
“MarcioJCosta10” <marciojcostadev@gmail.com>
2022-12-30 16:08:15 -0300

    update src/carts/models.py

--- carts/views.py
+++ carts/views.py
@@ -1,10 +1,7 @@
+
 from django.shortcuts import render
 from .models import Cart
 
-# Create your views here.
-
-
 def cart_home(request):
     cart_obj = Cart.objects.new_or_get(request)
-    context = {'cart_obj': cart_obj}
-    return render(request, "carts/home.html", context)
+    return render(request, "carts/home.html", {})
