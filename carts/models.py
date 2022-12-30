894351337a332fe8f3e38a51d2121e715d1c156d
“MarcioJCosta10” <marciojcostadev@gmail.com>
2022-12-30 15:41:19 -0300

    insert fiels object = CartManager() in class  model Cart

--- carts/models.py
+++ carts/models.py
@@ -36,6 +36,8 @@ class Cart(models.Model):
     total = models.DecimalField(default = 0.00, max_digits=100, decimal_places = 2)
     updated = models.DateTimeField(auto_now = True)
     timestamp = models.DateTimeField(auto_now_add = True)
+    
+    object = CartManager()
 
     def __str__(self):
         return str(self.id)
