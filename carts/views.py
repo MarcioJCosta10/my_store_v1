25af3f395868192a6cd878a1daa0a04727d650ae
“MarcioJCosta10” <marciojcostadev@gmail.com>
2022-12-30 16:19:13 -0300

    update src/carts/models.py

--- carts/views.py
+++ carts/views.py
@@ -4,4 +4,5 @@ from .models import Cart
 
 def cart_home(request):
     cart_obj = Cart.objects.new_or_get(request)
-    return render(request, "carts/home.html", {})
+    context = {'cart_obj': cart_obj}
+    return render(request, "carts/home.html", context)
