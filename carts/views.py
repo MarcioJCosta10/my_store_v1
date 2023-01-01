f2fb47ec500a0169a449f484cee02466e8c79b28
“MarcioJCosta10” <marciojcostadev@gmail.com>
2023-01-01 20:28:30 -0300

    test

--- carts/views.py
+++ carts/views.py
@@ -4,19 +4,5 @@ from .models import Cart
 
 
 def cart_home(request):
-
-    cart_obj, new_obj = Cart.objects.new_or_get(request)
-    products = cart_obj.products.all()
-    total = 0
-    for product in products:
-      total += product.price
-      print(total)
-      cart_obj.total = total
-      cart_obj.save()
-      
-    context = {
-      'cart_obj': cart_obj,
-      'total_cart' : cart_obj.total
-               }
-
-    return render(request, "carts/home.html", context)
+    cart_obj = Cart.objects.new_or_get(request)
+    return render(request, "carts/home.html", {})
