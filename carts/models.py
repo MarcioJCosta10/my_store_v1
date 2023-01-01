4521e63ed42cc2ad96bf1eab340d128e19eeee9b
“MarcioJCosta10” <marciojcostadev@gmail.com>
2023-01-01 19:44:53 -0300

    add call function pre_save.connect in src/carts/models.py

--- carts/models.py
+++ carts/models.py
@@ -63,3 +63,5 @@ m2m_changed.connect(m2m_changed_cart_receiver, sender=Cart.products.through)
 
 def pre_save_cart_receiver(sender, instance, *args, **kwargs):
   instance.total = instance.subtotal + 10 #Considere 0 10 como uma taxa de entrega
+  
+pre_save.connect(pre_save_cart_receiver, sender=Cart)
