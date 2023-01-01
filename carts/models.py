c96e2819410217a3bcbaa550d32b0a71b3d6685a
“MarcioJCosta10” <marciojcostadev@gmail.com>
2023-01-01 20:33:56 -0300

    update

--- carts/models.py
+++ carts/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.db import models
-
+from django.db.models.signals import pre_save, post_save, m2m_changed
 from products.models import Product
 
 User = settings.AUTH_USER_MODEL
@@ -12,7 +12,7 @@ class CartManager(models.Manager):
         if qs.count == 1:
             new_obj = False
             cart_obj = qs.first()
-            if request.user.is_authenticate and cart_obj.user is None:
+            if request.user.is_authenticated and cart_obj.user is None:
                 cart_obj.user = request.user
                 cart_obj.save()
         else:
@@ -20,22 +20,45 @@ class CartManager(models.Manager):
             new_obj = True
             request.session['cart_id'] = cart_obj.id
         return cart_obj, new_obj
-
     def new(self, user = None):
         user_obj = None
         if user is not None:
             if user.is_authenticated:
                 user_obj = user
         return self.model.objects.create(user = user_obj)
-      
+
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null = True, blank = True)
     products = models.ManyToManyField(Product, blank = True)
     total = models.DecimalField(default = 0.00, max_digits=100, decimal_places = 2)
+    subtotal = models.DecimalField(default = 0.00, max_digits=100, decimal_places = 2)
     updated = models.DateTimeField(auto_now = True)
     timestamp = models.DateTimeField(auto_now_add = True)
-    
+
     objects = CartManager()
-    
+
     def __str__(self):
         return str(self.id)
+
+def m2m_changed_cart_receiver(sender, instance, action, *args, **kwargs):
+  #print(action)
+  if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+    #print(instance.products.all())
+    #print(instance.total)
+    products = instance.products.all() 
+    total = 0 
+    for product in products: 
+      total += product.price 
+    if instance.subtotal != total:
+      instance.subtotal = total
+      instance.save()
+    #print(total) 
+    instance.subtotal = total
+    instance.save()
+
+m2m_changed.connect(m2m_changed_cart_receiver, sender = Cart.products.through)
+
+def pre_save_cart_receiver(sender, instance, *args, **kwargs):
+  instance.total = instance.subtotal + 10 # considere o 10 como uma taxa de entrega
+
+pre_save.connect(pre_save_cart_receiver, sender = Cart)
--- carts/views.py
+++ carts/views.py
@@ -1,12 +1,7 @@
-from django.shortcuts import render
 
+from django.shortcuts import render
 from .models import Cart
 
-
 def cart_home(request):
-
-    cart_obj = Cart.objects.new_or_get(request)
-
-    context = {'cart_obj': cart_obj}
-
-    return render(request, "carts/home.html", context)
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    return render(request, "carts/home.html", {})
