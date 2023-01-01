b97768b829e4db467c5914c195fc1ee0b91cb3ea
“MarcioJCosta10” <marciojcostadev@gmail.com>
2023-01-01 19:11:43 -0300

    insert variable new_obj in function cart_home insert variable products and variable total run loop for sun total in src/carts/views.py

--- carts/views.py
+++ carts/views.py
@@ -5,8 +5,18 @@ from .models import Cart
 
 def cart_home(request):
 
-    cart_obj = Cart.objects.new_or_get(request)
-
-    context = {'cart_obj': cart_obj}
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    products = cart_obj.products.all()
+    total = 0
+    for product in products:
+      total += product.price
+      print(total)
+      cart_obj.total = total
+      cart_obj.save()
+      
+    context = {
+      'cart_obj': cart_obj,
+      'total_cart' : cart_obj.total
+               }
 
     return render(request, "carts/home.html", context)
