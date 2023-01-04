e0e0746c374a660a3f914854d4cb31e0baabd7db
“MarcioJCosta10” <marciojcostadev@gmail.com>
2023-01-03 22:27:31 -0300

    update

--- carts/views.py
+++ carts/views.py
@@ -6,7 +6,6 @@ from .models import Cart
 def cart_home(request):
     cart_obj = Cart.objects.new_or_get(request)
     return render(request, "carts/home.html", {})
-<<<<<<< HEAD
   
 def cart_update(request):  
     product_id = 5
@@ -14,7 +13,6 @@ def cart_update(request):
     product_obj = Product.objects.get(id=product_id)
     # Create or get instance cart if already exists
     cart_obj, new_obj = Cart.objects.new_or_get(request)
-  
     if product_obj in cart_obj.products.all():
       cart_obj.products.remove(product_obj) # cart_obj.products.remove(product_id)
     else:  
@@ -25,20 +23,3 @@ def cart_update(request):
       #let's  use namespace cart
     return redirect("cart:home")
   
-=======
-
-def cart_update(request):
-    product_id = 5
-    # Pega o produto com id 5
-    product_obj = Product.objects.get(id=product_id)
-    # Cria ou pega a instância já existente do carrinho
-    cart_obj, new_obj = Cart.objects.new_or_get(request)
-    if product_obj in cart_obj.products.all():
-        cart_obj.products.remove(product_obj) # cart_obj.products.remove(product_id)
-    else:
-        # E o produto se adiciona a instância do campo M2M 
-        cart_obj.products.add(product_obj) # cart_obj.products.add(product_id)
-    
-    # retorna redirect(product_obj.get_absolute_url())
-    return redirect("cart:home")
->>>>>>> 44_update_carts_in_frontend
