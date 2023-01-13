d21cd8e062bc2c44a043961e1584152b75b541fc
“MarcioJCosta10” <marciojcostadev@gmail.com>
2023-01-13 20:58:39 -0300

    use module math and function fsum for check compatibility of types

--- orders/models.py
+++ orders/models.py
@@ -27,7 +27,11 @@ class Order(models.Model):
     def update_total(self):
         cart_total = self.cart.total
         shipping_total = self.shipping_total
-        new_total = float(cart_total) + float(shipping_total)
+        #new_total = float(cart_total) + float(shipping_total)
+        #new_total = cart_total + shipping_total
+        new_total = math.fsum([cart_total, shipping_total])
+        formatted_total = format(new_total, '.2f')
+        self.total = formatted_total 
         self.total = new_total
         self.save()
         return new_total
