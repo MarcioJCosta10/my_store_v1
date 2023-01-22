b0411f101aaed18ad4001988b489dfa8cd599f66
“MarcioJCosta10” <marciojcostadev@gmail.com>
2023-01-22 09:50:05 -0300

    create BillienProfileManger in src/billing/models.py

--- billing/models.py
+++ billing/models.py
@@ -7,6 +7,24 @@ from accounts.models import GuestEmail
 User = settings.AUTH_USER_MODEL
 # fulano@mail.com -> pode ter 1.000.000.000 billing profiles
 # user fulano@mail.com -> pode ter apenas 1 billing profile
+class BillingProfileManager(models.Manager):
+    def new_or_get(self, request):
+        user = request.user
+        guest_email_id = request.session.get('guest_email_id')
+        created = False
+        obj = None
+        if user.is_authenticated:
+            'logged in user checkout; remember payment stuff'
+            obj, created = self.model.objects.get_or_create(
+                            user=user, email=user.email)
+        elif guest_email_id is not None:
+            'guest user checkout; auto reloads payment stuff'
+            guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
+            obj, created = self.model.objects.get_or_create(
+                                            email=guest_email_obj.email)
+        else:
+            pass
+        return obj, created
 class BillingProfile(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete = models.CASCADE)
     email = models.EmailField()
