64bd744985101f4685d0e793f0ea664204d8b17d
“MarcioJCosta10” <marciojcostadev@gmail.com>
2023-01-24 22:50:44 -0300

    create fields shipping_adress and billing_address in class Order in src/orders/models.py

--- orders/models.py
+++ orders/models.py
@@ -35,8 +35,8 @@ class Order(models.Model):
     billing_profile = models.ForeignKey(BillingProfile, on_delete=models.CASCADE, null = True, blank = True)
     order_id = models.CharField(max_length = 120, blank = True)
     # billing_profile = ?
-    # shipping_address = ?
-    # billing_address
+    shipping_address = models.ForeignKey(Address, related_name="shipping_address", on_delete=models.CASCADE, null=True, blank=True)
+    billing_address = models.ForeignKey(Address, related_name="billing_address", on_delete=models.CASCADE, null=True, blank=True)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null = True)
     status = models.CharField(max_length = 120, default = 'created', choices = ORDER_STATUS_CHOICES )
     shipping_total = models.DecimalField(default = 5.99, max_digits = 100, decimal_places = 2)
