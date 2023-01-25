fcdc197e2ffab753a84c033fb585273a3c24c131
“MarcioJCosta10” <marciojcostadev@gmail.com>
2023-01-24 22:48:01 -0300

    up

--- carts/views.py
+++ carts/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
+
 from accounts.forms import LoginForm, GuestForm
 from accounts.models import GuestEmail
 from addresses.forms import AddressForm
+
 from billing.models import BillingProfile
 from orders.models import Order
 from products.models import Product
 from .models import Cart
-
 
 def cart_home(request):
     cart_obj, new_obj  = Cart.objects.new_or_get(request)
@@ -28,7 +29,7 @@ def cart_update(request):
             cart_obj.products.add(product_obj)
         request.session['cart_items'] = cart_obj.products.count()
     return redirect("cart:home")
-  
+
 def checkout_home(request):
     #aqui a gente pega o carrinho
     cart_obj, cart_created= Cart.objects.new_or_get(request)
@@ -37,13 +38,13 @@ def checkout_home(request):
     #ou se o carrinho já existir mas não tiver nada dentro
     if cart_created or cart_obj.products.count() == 0:
         return redirect("cart:home")
-      
+    
     login_form = LoginForm()
     guest_form = GuestForm()
     address_form = AddressForm()
 
     billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
-    
+
     if billing_profile is not None:
         order_obj, order_obj_created = Order.objects.new_or_get(billing_profile, cart_obj)
 
@@ -51,6 +52,7 @@ def checkout_home(request):
         "object": order_obj,
         "billing_profile": billing_profile,
         "login_form": login_form,
-        "guest_form": guest_form
+        "guest_form": guest_form,
+        "address_form": address_form,
     }
     return render(request, "carts/checkout.html", context)
