9752f68f0207a8f0de15c81b49c054d44adcc75c
“MarcioJCosta10” <marciojcostadev@gmail.com>
2023-01-25 21:12:23 -0300

    update method update_total and create check_done and mark_paid in src/orders/models.py

--- orders/models.py
+++ orders/models.py
@@ -2,11 +2,10 @@
 import math
 from django.db import models
 from django.db.models.signals import pre_save, post_save
-from carts.models import Cart
-from billing.models import BillingProfile
 from addresses.models import Address
-from my_store.utils import unique_order_id_generator
-
+from billing.models import BillingProfile
+from carts.models import Cart
+from e_commerce.utils import unique_order_id_generator
 
 ORDER_STATUS_CHOICES = (
     ('created', 'Criado'),
@@ -14,21 +13,20 @@ ORDER_STATUS_CHOICES = (
     ('shipped', 'Enviado'),
     ('refunded', 'Devolvido'),
 )
-
 class OrderManager(models.Manager):
     def new_or_get(self, billing_profile, cart_obj):
         created = False
         qs = self.get_queryset().filter(
-                billing_profile = billing_profile, 
-                cart = cart_obj, 
-                active = True,
+                billing_profile=billing_profile, 
+                cart=cart_obj, 
+                active=True, 
                 status='created'
-                )
+            )
         if qs.count() == 1:
             obj = qs.first()
         else:
             obj = self.model.objects.create(
-                    billing_profile = billing_profile, 
+                    billing_profile=billing_profile, 
                     cart=cart_obj)
             created = True
         return obj, created
@@ -47,25 +45,38 @@ class Order(models.Model):
 
     def __str__(self):
         return self.order_id
-      
+
     objects = OrderManager()
-    
+
     def update_total(self):
         cart_total = self.cart.total
         shipping_total = self.shipping_total
-        #new_total = cart_total + shipping_total
         new_total = math.fsum([cart_total, shipping_total])
         formatted_total = format(new_total, '.2f')
         self.total = formatted_total 
         self.save()
         return new_total
+    def check_done(self):
+        billing_profile = self.billing_profile
+        shipping_address = self.shipping_address
+        billing_address = self.billing_address
+        total = self.total
+        if billing_profile and shipping_address and billing_address and total > 0:
+            return True
+        return False
 
+    def mark_paid(self):
+        if self.check_done():
+            self.status = "paid"
+            self.save()
+        return self.status
 def pre_save_create_order_id(sender, instance, *args, **kwargs):
     if not instance.order_id:
         instance.order_id = unique_order_id_generator(instance)
-     # retorna todos os orders com esta instância de cart, 
-    # excluindo aqueles que têm a mesma instância de billing_profile
+    # return all orders with this Cart instance, excluding those  
+    # that have the same billing profile instance
     qs = Order.objects.filter(cart = instance.cart).exclude(billing_profile = instance.billing_profile)
+    print("QuerySet: ", qs)
     if qs.exists():
         qs.update = False
 
@@ -89,4 +100,4 @@ def post_save_order(sender, instance, created, *args, **kwargs):
         print("Atualizando")
         instance.update_total()
 
-post_save.connect(post_save_order, sender=Order)
+post_save.connect(post_save_order, sender=Order) 
