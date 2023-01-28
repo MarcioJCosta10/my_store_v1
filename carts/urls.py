4fc0d3b678d291caab8f2fdb301af0308e71ca72
“MarcioJCosta10” <marciojcostadev@gmail.com>
2023-01-28 17:12:24 -0300

    create route checkout/success/ in src/carts/urls.py

--- carts/urls.py
+++ carts/urls.py
@@ -5,11 +5,14 @@ app_name = 'carts'
 from .views import (
                     cart_home,
                     checkout_home,
-                    cart_update  
+                    cart_update,
+                    checkout_done_view
+ 
 )
 
 urlpatterns = [
   path('', cart_home, name='home'),
   path('checkout/', checkout_home, name='checkout'),
-  path('update/', cart_update, name='update')
+  path('update/', cart_update, name='update'),
+  path('checkout/success/', checkout_done_view, name='success'),
 ]
