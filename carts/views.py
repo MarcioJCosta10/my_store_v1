06fe12e5a56a42e5d83fbcd0ed645865f6b24912
“MarcioJCosta10” <marciojcostadev@gmail.com>
2023-02-04 22:15:53 -0300

    insert request ajax and response

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 
 from accounts.forms import LoginForm, GuestForm
 from accounts.models import GuestEmail
@@ -14,11 +15,14 @@ from .models import Cart
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
     return render(request, "carts/home.html", {"cart": cart_obj})
+  
+def is_ajax(request):
+    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
 
 def cart_update(request):
     product_id = request.POST.get('product_id')
-    if request.is_ajax():
-      print("Ajax request")
+    # if request.is_ajax():
+    #   print("Ajax request")
     if product_id is not None:
         try:
             product_obj = Product.objects.get(id=product_id)
@@ -28,10 +32,19 @@ def cart_update(request):
         cart_obj, new_obj = Cart.objects.new_or_get(request)
         if product_obj in cart_obj.products.all():
             cart_obj.products.remove(product_obj)
+            added = False
         else:
             cart_obj.products.add(product_obj) # cart_obj.products.add(product_id)
+            added = True
         request.session['cart_items'] = cart_obj.products.count()
         # return redirect(product_obj.get_absolute_url())
+        if is_ajax(request=request):
+            print("Ajax request")
+            json_data = {
+                "added": added,
+                "removed": not added,
+            }
+        return JsonResponse(json_data)
     return redirect("cart:home")
 
 def checkout_home(request):
