c1c71067f1cd6bbb1a47bc30500c91131c4e668d
“MarcioJCosta10” <marciojcostadev@gmail.com>
2023-06-10 19:23:23 -0300

    import forms UserAdminCreationForm and UserAdminChangeForm in src/accounts/admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,6 +4,9 @@ from .models import GuestEmail
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
+
+from .forms import UserAdminCreationForm, UserAdminChangeForm
+
 User = get_user_model()
 admin.site.register(User)
 admin.site.register(GuestEmail)
